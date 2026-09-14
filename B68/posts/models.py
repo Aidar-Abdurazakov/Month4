011d99030f78f6f809b64562cb6cab39a0a79e24
Aidar-Abdurazakov <aidarabdurazakov05@outlook.com>
2026-09-14 23:57:37 +0600

    last commit

---
+++ B68/posts/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    text = models.CharField(max_length=5000)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True
+    )
+    tags = models.ManyToManyField(Tag)
+    image = models.ImageField(null=True, upload_to="posts")
+    views = models.IntegerField(default=0)
+
+
+class Comment(models.Model):
+    authoer_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    text = models.TextField()
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+
+    def __str__(self):
+        return self.text
---
+++ B68/posts/views.py
@@ -0,0 +1,59 @@
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
+from posts.forms import PostForm
+from posts.models import Category, Post, Tag
+
+
+def hello_world(r):
+    return HttpResponse("<h1>Hello world!</h1>")
+
+
+def my_name(r):
+    name = "Islam"
+
+    return HttpResponse(f"<h2> Hello </h2> <h1>{name}</h1>")
+
+
+
+def say_name(r, name):
+    return HttpResponse(f"<h2> Hello </h2> <h1>{name}</h1>")
+
+
+def post_list(r):
+    posts = Post.objects.all()
+
+    return render(r, "posts/list_posts.html", {"posts": posts})
+
+
+def post_detail(r, pk):
+    post = get_object_or_404(Post, id=pk)
+    post.views += 1
+    post.save()
+
+    return render(r, "posts/post_detail.html", {"post": post})
+
+
+def create_post(request: HttpRequest) -> HttpResponse:
+    form = PostForm()
+
+    if request.method.lower() == "post":
+        form = PostForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect("post_detail", pk=form.instance.pk)
+
+    tags = Tag.objects.all()
+    categories = Category.objects.all()
+
+    return render(
+        request,
+        "posts/create_post.html",
+        {
+            "form": form,
+            "tags": tags,
+            "categories": categories
+        },
+    )
